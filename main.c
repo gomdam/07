7dbba8ba67dd73a4f9245797462c9f242188fbe4
gommdamm <goyoudam2357@gmail.com>
2025-10-16 15:37:03 +0900

    06-3

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int inc(int counter)
+{
+	counter++;
+	return counter;
+}
+
 int main(int argc, char *argv[]) 
 {
-	int i;
-	int temp=1;
-	
-	for(i=0;i<5;i++)
-	{
-		printf("temp = %d\n", temp);
-		temp++;
-	}
+	int i=10;
+	printf("before i = %d\n", i);
+	i = inc(i); //함수의 반환값 이용 
+	printf("after i = %d\n", i);
 	return 0;
 }
